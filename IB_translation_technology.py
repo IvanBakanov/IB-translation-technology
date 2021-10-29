6951bbddc013f01ba4679d484c511d4c74da87fb
IvanBakanov <66953937+IvanBakanov@users.noreply.github.com>
2021-10-29 17:22:28 +0300

    Update IB_translation_technology.py

--- IB_translation_technology.py
+++ IB_translation_technology.py
@@ -16,7 +16,7 @@ translator=google_translator()
 print(Back.BLUE+'https://github.com/IvanBakanov/IB-translation-technology')
 print(Style.RESET_ALL+'version 0.02\n')
 
-dest_language = input('Destination language: ')
+dest_lang = input('Destination language: ')
 
 dir_name = filedialog.askdirectory()
 screenshot_path = dir_name+'\\screenshot.png'
@@ -31,12 +31,12 @@ def on_press_func(key):
             cropped.save(screenshot_path)
             text = image_to_string(Image.open(screenshot_path))
             for word in text.split():
-                src_language = translator.detect(word)[0]
-                result = translator.translate(word, lang_src=src_language, lang_tgt=dest_language)
+                src_lang = translator.detect(word)[0]
+                result = translator.translate(word, lang_src=src_lang, lang_tgt=dest_lang)
                 print('\n'+word+' -> '+result)
     except: pass
 
-if dest_language in LANGUAGES.keys() and dir_name:      
+if dest_lang in LANGUAGES.keys() and dir_name:      
     listener = keyboard.Listener(on_press=on_press_func)
     listener.start()
     listener.join()
